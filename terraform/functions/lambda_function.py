d1385ff7591a6a545402ab28345b395d1a466b16
ensono-markanthony <mark.anthony@ensono.com>
2021-06-22 10:25:41 +0100

    updated formatting of lambda code

--- terraform/functions/lambda_function.py
+++ terraform/functions/lambda_function.py
@@ -7,7 +7,6 @@ print('Loading function')
 
 s3 = boto3.client('s3')
 
-
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
@@ -18,30 +17,27 @@ def lambda_handler(event, context):
     s3object = record['s3']['object']['key']
 
     try:
-        response = s3.get_object(Bucket=s3bucket, Key=s3object)
-        print("CONTENT TYPE: " + response['ContentType'])
+      response = s3.get_object(Bucket=s3bucket, Key=s3object)
+      print("CONTENT TYPE: " + response['ContentType'])
 
-        # Setting object content to var, ready for input to piexif.
-        content = response['Body'].read()
+      #Setting object content to var, ready for input to piexif.
+      content = response['Body'].read()
 
-        print('DOING THE EXIF REMOVAL')
-        # Writing to memory, due to no writable storage in lambda.
-        output = io.BytesIO()
+      print('DOING THE EXIF REMOVAL')
+      #Writing to memory, due to no writable storage in lambda.
+      output = io.BytesIO()
 
-        # Running piexif to remove exif data.
-        newImage = piexif.remove(content, output)
+      #Running piexif to remove exif data.
+      newImage = piexif.remove(content,output)
 
-        # Upload Object to bucket
-        objectupload = s3.put_object(
-            Body=output,
-            Bucket='bucket-images-noexif',
-            Key=s3object)
-
+      #Upload Object to bucket
+      objectupload = s3.put_object(
+        Body=output,
+        Bucket='bucket-images-noexif',
+        Key=s3object)
 
     except Exception as e:
-        print(e)
-        print(
-            'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
-                s3object, s3bucket))
-        raise e
+      print(e)
+      print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(s3object, s3bucket))
+      raise e
 
